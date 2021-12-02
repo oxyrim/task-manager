fc664bda1265c045c7100c85c6ef60f1af5fb046
jmeth <jmethukten@gmail.com>
2021-12-02 18:52:17 -0500

    Hash password/bcrypt and save user credentials to DB

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
 
 // @route       POST api/users
@@ -17,12 +18,35 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ message: 'User already exist.' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const saltRounds = 10;
+
+      user.password = await bcrypt.hash(password, saltRounds);
+      await user.save();
+      res.send('User save to DB');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: 'Server error' });
+    }
   }
 );
 
