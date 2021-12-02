089ee4b1ada299893ceb729b132b79e7f749fb7b
jmeth <jmethukten@gmail.com>
2021-12-02 17:12:41 -0500

    Define UserSchema and setup user model

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 // Register Route
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
 router.post('/', (req, res) => {
-  res.send('Regsiter user');
+  res.send(req.body);
 });
 
 module.exports = router;
