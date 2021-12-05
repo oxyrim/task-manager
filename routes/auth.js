2954b33e5124b9ef24c7843302c4ca4101ac4e73
jmeth <jmethukten@gmail.com>
2021-12-04 19:59:52 -0500

    Register login/authentication route, check user credentials  and generate jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 // login, authentication and route check to logged in users
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route       GET api/auth
 // @desc        Get logged in users
@@ -12,8 +17,44 @@ router.get('/', (req, res) => {
 // @route       POST api/auth
 // @desc        Authenticate user and get token
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Login user');
-});
+router.post(
+  '/',
+  [
+    body('email', 'Please enter email').isEmail(),
+    body('password', 'Please enter password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let foundUser = await User.findOne({ email });
+      if (!foundUser) {
+        return res.status(400).json({ message: 'Invalid email/password' });
+      }
+      bcrypt.compare(password, foundUser.password, (error, result) => {
+        if (!result) {
+          return res.status(400).json({ message: 'Invalid email/password' });
+        }
+
+        jwt.sign(
+          { user: { id: foundUser.id } },
+          config.get('JWT_SECRET'),
+          (error, token) => {
+            if (error) throw error;
+            res.json({ token });
+          }
+        );
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.stauts(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
