574d067acf56427e89bee018b899e8b44facb8f2
jmeth <jmethukten@gmail.com>
2021-12-17 18:57:19 -0500

    Install and setup react and concurrently

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
